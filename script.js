77a9e650495fbdad96c7d7c3edc275fedb418d7a
André Dias <andremssdias@hotmail.com>
2024-03-13 23:01:42 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        // Ask user to select window
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play()
+        }
+
+
+
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+button.addEventListener("click", async () => {
+    // Disable Button
+    button.disabled = true;
+
+    // Start Picture in Picture
+    await videoElement.requestPictureinPicture();
+
+    // Reset Button
+    button.disabled = false;
+})
+
+selectMediaStream()
